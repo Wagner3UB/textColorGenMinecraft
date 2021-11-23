38031e087104a3e9d60d44926816351740d0334a
Wagner Trezub <trezub.w@gmail.com>
2021-11-23 11:29:32 +0100

    inicial JS

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let textArea = document.querySelector(".texts textarea").innerHTML;
+
+const itemValue = (button) => {
+  let listButtons = document.querySelector(".options");
+  let oneButton = listButtons.getElementsByTagName("button");
+  for (i=0; i<oneButton.length; i++ ){
+    oneButton[i].value;
+  }
+  let buttonValue = button.value;
+  textArea += buttonValue;
+  document.querySelector(".texts textarea").innerHTML = textArea;
+  console.log(buttonValue);
+
+  switch (buttonValue) {
+    case "&0":
+      document.querySelector(".preview p").innerHTML += '<span class="0"><span>'; 
+      break;
+    case "&1":
+      document.querySelector(".preview p").innerHTML += '<span class="1"><span>'; 
+      break;
+
+  }
+
+};
+
