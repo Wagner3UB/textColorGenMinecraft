c6fa8020b549cf12dbbef6729f213f6810eb9be4
Wagner Trezub <trezub.w@gmail.com>
2021-11-24 10:33:12 +0100

    New Branch version2 with old idea

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const itemValue = (button) => {
   }
   let buttonValue = button.value;
   textArea += buttonValue;
+  document.querySelector(".texts textarea").innerHTML = textArea;
   console.log(buttonValue);
 
   switch (buttonValue) {
@@ -20,7 +21,23 @@ const itemValue = (button) => {
 
   }
 };
-const letterKeyboard = () =>{
+const letterKeyboard = (item) =>{
+  item = item || window.event;
+  let code = item.which || item.keyCode;
+  console.log(code);
+  console.log(String.fromCharCode(code));
 
+  let valueKey = document.querySelector("textarea").value;
+  if(code == 8){
+    let tamanho = document.querySelector(".preview p").innerHTML.length;
+    document.querySelector(".preview p").innerHTML = document.querySelector(".preview p").innerHTML.slice(0,--tamanho);
+    
+  } else {
+    if(document.querySelector(".preview p").innerHTML == ''){
+    document.querySelector(".preview p").innerHTML += valueKey;
+    } else {
+    document.querySelector(".preview p").innerHTML += String.fromCharCode(code);
+    }
+  }
 }
 
