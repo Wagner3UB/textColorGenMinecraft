e25383deb6ace64f16775ff87ce743b28e5942b7
Wagner Trezub <trezub.w@gmail.com>
2021-11-24 10:22:12 +0100

    testing new branch

--- js/script.js
+++ js/script.js
@@ -20,24 +20,7 @@ const itemValue = (button) => {
 
   }
 };
-const letterKeyboard = (item) =>{
-  item = item || window.event;
-  let code = item.which || item.keyCode;
-  console.log(code);
-  console.log(String.fromCharCode(code));
+const letterKeyboard = () =>{
 
-  let valueKey = document.querySelector("textarea").value;
-  if(code == 8){
-    let tamanho = document.querySelector(".preview p").innerHTML.length;
-    document.querySelector(".preview p").innerHTML = document.querySelector(".preview p").innerHTML.slice(0,--tamanho);
-  } else {
-    if(document.querySelector(".preview p").innerHTML == ''){
-      document.querySelector(".preview p").innerHTML += valueKey;
-  } else {
-      document.querySelector(".preview p").innerHTML += String.fromCharCode(code);
-  }
-  }
-
-  
 }
 
